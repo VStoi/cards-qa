e4a7fe14d9e5117e43b9dde04517fed3c07a23fb
Vadim Stojlovskij <vadim.stojlovskij@murka.com>
2020-10-22 22:37:53 +0300

    Update tests

--- src/test/java/org/cards/ui/pages/HomePage.java
+++ src/test/java/org/cards/ui/pages/HomePage.java
@@ -1,6 +1,5 @@
 package org.cards.ui.pages;
 
-import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.*;
--- src/test/java/org/cards/ui/tests/BasePageTest.java
+++ src/test/java/org/cards/ui/tests/BasePageTest.java
@@ -1,9 +1,12 @@
 package org.cards.ui.tests;
 
 import com.codeborne.selenide.WebDriverRunner;
+import org.cards.ui.helpers.db.verification_tokens.VerificationTokensDB;
 import org.cards.ui.pages.HomePage;
 import org.cards.ui.pages.SignUpPage;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 
@@ -19,9 +22,14 @@ public class BasePageTest {
 
     @Test
     public void testFormFill() {
-        SignUpPage signUpPage = new SignUpPage();
-        signUpPage.fillForm("user@test.com",
+        open("http://localhost:3000/sign-up");
+        SignUpPage page = new SignUpPage();
+        page.fillForm("user@test.com",
                 "Qwerty11",
                 "Qwerty11");
+        VerificationTokensDB db = new VerificationTokensDB("user@test.com");
+        page.fillTokenAndSubmit(db.getToken());
+        String url = WebDriverRunner.url();
+        assertEquals("http://localhost:3000/sign-in", url);
     }
 }
